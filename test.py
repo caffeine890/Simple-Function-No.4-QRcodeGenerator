a26531102aa1f481dffeb4e0961fa13165895880
DESKTOP-UCP5P1F\caffeine111 <kaminari8`icloud.com>
2024-03-16 23:55:41 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,9 +1,9 @@
 import qrcode
 
-# QRコードに含めるデータ
+# Data to include in the QR code
 data = "https://www.google.co.jp/"
 
-# QRコード生成
+# Generate QR code
 qr = qrcode.QRCode(
     version=1,
     error_correction=qrcode.constants.ERROR_CORRECT_L,
@@ -13,8 +13,8 @@ qr = qrcode.QRCode(
 qr.add_data(data)
 qr.make(fit=True)
 
-# QRコードを画像として生成
+# Generate QR code as an image
 img = qr.make_image(fill_color="black", back_color="white")
 
-# 画像をファイルに保存
+# Save the image to a file
 img.save("qr_code_example.png")
